e9bd94df7f842e965885f378db2c2c393f209001
evgeniakalinina <evg5408@yandex.ru>
2021-03-11 13:30:14 +0300

    задачка про квадраты

--- file_1.py
+++ file_1.py
@@ -1,1 +1,15 @@
-...
+from typing import List
+"""1277. Count Square Submatrices with All Ones"""
+f = [[1, 1, 1, 1], [0, 1, 1, 0], [0, 1, 1, 1]]
+s = []
+def countSquares(f: List[List[int]]) -> int:
+    for _ in range(len(f)):
+        print(f[_])
+    h = len(f)
+
+    for i in range(len(f)-1):
+        for j in range(len(f[0])-1):
+            a = f[i][j]+f[i+1][j] +f[i][j+1] +f[i+1][j+1]
+            s.append(a)
+    return s
+print(countSquares(f))
